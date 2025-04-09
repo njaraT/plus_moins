016e2758939ead1047022048e4e0da7d9260c83e
njaraT <njaratianarandriambola4@gamil.com>
2025-04-09 12:52:26 +0400

    Version 0.2 : ajout des difficultes

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -5,16 +5,46 @@
 void plus_moins() {
     int nombreMystere, nombreUtilisateur;
     int essais = 0;
+    int maxEssais = -1;
+    int niveau;
 
-    // Initialisation de l'aléatoire
-    srand(time(NULL));
-    nombreMystere = rand() % 100 + 1; // Nombre entre 1 et 100
-
+    // Choix du niveau de difficulté
     printf("=== Jeu du Plus ou Moins ===\n");
-    printf("J'ai choisi un nombre entre 1 et 100. À vous de deviner !\n");
+    printf("Choisissez un niveau de difficulté :\n");
+    printf("1 - Facile (illimité)\n");
+    printf("2 - Moyen (25 essais)\n");
+    printf("3 - Difficile (10 essais)\n");
+    printf("Votre choix : ");
+    scanf("%d", &niveau);
+
+    switch (niveau) {
+        case 1:
+            maxEssais = -1; // illimité
+            break;
+        case 2:
+            maxEssais = 25;
+            break;
+        case 3:
+            maxEssais = 10;
+            break;
+        default:
+            printf("Niveau invalide. Le niveau facile sera utilisé par défaut.\n");
+            maxEssais = -1;
+            break;
+    }
+
+    // Initialisation du nombre aléatoire
+    srand(time(NULL));
+    nombreMystere = rand() % 100 + 1;
+
+    printf("J'ai choisi un nombre entre 1 et 100. Essayez de le deviner !\n");
 
     do {
-        printf("Votre proposition : ");
+        printf("Essai %d", essais + 1);
+        if (maxEssais > 0) {
+            printf(" / %d", maxEssais);
+        }
+        printf(" - Votre proposition : ");
         scanf("%d", &nombreUtilisateur);
         essais++;
 
@@ -24,9 +54,15 @@ void plus_moins() {
             printf("C'est moins !\n");
         } else {
             printf("Bravo ! Vous avez trouvé le nombre mystère en %d essai(s) !\n", essais);
+            return;
         }
 
-    } while (nombreUtilisateur != nombreMystere);
+        if (maxEssais > 0 && essais >= maxEssais) {
+            printf("Dommage ! Vous avez atteint la limite d'essais. Le nombre mystère était : %d\n", nombreMystere);
+            return;
+        }
+
+    } while (1);
 }
 
 int main() {
